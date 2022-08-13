9a8fb56b68547258ef91dcf5c81eae9974b56ae0
hwangiris <42462259+hwangiris@users.noreply.github.com>
2022-08-13 19:00:20 +0800

    update current page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,7 @@
 $(function() {
   const current = window.location.pathname.split('/').pop();
-  console.log(current);
   $('nav a').each(function(){
     let href = $(this).attr('href');
-    console.log(href);
     if ( href.indexOf(current) > 0 ) {
       $(this).addClass('active');
     }
