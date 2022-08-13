d45b157f3f66b028be843e3899d031089a5b4368
hwangiris <42462259+hwangiris@users.noreply.github.com>
2022-08-13 19:12:53 +0800

    update current page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,12 +1,10 @@
-$(function() {
-  let current = window.location.pathname.split('/').pop();
-  if ( current === '' ) {
-    current = 'index.html';
+let current = window.location.pathname.split('/').pop();
+if ( current === '' ) {
+  current = 'index.html';
+}
+$('nav a').each(function(){
+  let href = $(this).attr('href');
+  if ( href.indexOf(current) > 0 ) {
+    $(this).addClass('active');
   }
-  $('nav a').each(function(){
-    let href = $(this).attr('href');
-    if ( href.indexOf(current) > 0 ) {
-      $(this).addClass('active');
-    }
-  })
-});
+})
