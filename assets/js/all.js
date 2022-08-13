27effd8a8701394a6c775a60ba2da23515a4749b
hwangiris <42462259+hwangiris@users.noreply.github.com>
2022-08-13 18:33:11 +0800

    Update 2022-08-13T10:33:08.026Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,13 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  var current = window.location.pathname;
+  $('nav a').each(function () {
+    var href = $(this).attr('href');
+
+    if (href.indexOf(current) > 0) {
+      $(this).addClass('active');
+    }
+  });
 });
 //# sourceMappingURL=all.js.map
